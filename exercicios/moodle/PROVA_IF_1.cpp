6a2b1c8d6f2d615429cde9084148423fdbcc9850
Luis Pedro Costa <luispedroccosta@gmail.com>
2026-03-12 11:30:45 -0300

    exercícios moodle

---
+++ exercicios/moodle/PROVA_IF_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    float p = 0; //peso
+    float h = 0; //altura
+    cin >> p >> h;
+
+    float imc = 0;
+    imc = p/pow(h,2);
+
+    if(imc < 18.5){
+        cout << "Baixo peso" << endl;
+    } else if((imc >= 18.5) && (imc < 25)){
+        cout << "Normal";
+    } else if((imc >= 25) && (imc < 30)){
+        cout << "Sobrepeso";
+    }
+    return 0;
+}
