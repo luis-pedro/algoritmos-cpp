26b7868ec5797d19c959032c6e594c2bc9b875ae
Luis Pedro Costa <luispedroccosta@gmail.com>
2026-04-13 16:45:52 -0300

    Exercícios do Moodle

--- exercicios/moodle/pesquisa.cpp
+++ exercicios/moodle/pesquisa.cpp
@@ -3,15 +3,35 @@
 using namespace std;
 
 int main(){
-    int N[100];
+    int N[100]; //Vetor para armazenar os números
+    int V = 0; //Variável do número de verificação
+    int i = 0; //Variável auxiliar para o loop
+    int A = 0; //Variável auxiliar
 
-    cin >> N[0];
+    cin >> N[A];
+    cout << "Elemento " << N[0] << " armazenado na posicao " << A << endl;
 
-    while(N[0] != 0){
-        cin >> N[0];
+    // Continua lendo os números até que o primeiro número seja 0
+    while(N[A] != 0){
+        A += 1;
+        cin >> N[A];
+        cout << "Elemento " << N[A] << " armazenado na posicao " << A << endl;
     }
 
-    cout << N[0] << endl;
+    // Verifica se o número de verificação está no vetor
+    cin >> V;
+    cout << "Numero de verificacao: " << V << endl;
+    cout << N[A] << endl;
+    for(i = 0; i < A; i++){
+        if(N[i] == V){
+            cout << "Elemento " << V << " encontrado na posicao " << i << endl;
+            break;
+        } 
+    }
+
+    if(i==A){
+        cout << "Elemento " << V << " nao encontrado" << endl;
+    }
 
     return 0;
 }
