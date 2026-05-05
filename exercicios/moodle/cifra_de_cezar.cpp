ab1b9a37784e8c2dd88bd28cfb858a2974d20900
Luis Pedro Costa <luispedroccosta@gmail.com>
2026-05-04 22:47:17 -0300

    Exercícios Moodle

--- exercicios/moodle/cifra_de_cezar.cpp
+++ exercicios/moodle/cifra_de_cezar.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-    
+    int N = 3; // Número de linhas
+    int M = 5; // Número de colunas
+    string vet[3][5];
+
+    for(int i = 0 ; i < N ; i++){
+        for(int j = 0 ; j < M ; j++){
+            string(cin >> vet[i][j],ws);
+        }
+    }
+
+    for(int i = 0 ; i < N ; i++){
+        for(int j = 0 ; j < M ; j++){
+            cout << vet[i][j] << " ";
+        }
+    }
 
     return 0;
 }
