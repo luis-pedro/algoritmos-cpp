e31cdbb10a3f8888e332b8ac75fe7f5ef8194f00
Luis Pedro Costa <luispedroccosta@gmail.com>
2026-05-07 13:37:37 -0300

    Exercícios Moodle

--- exercicios/moodle/ocr.cpp
+++ exercicios/moodle/ocr.cpp
@@ -1,23 +1,90 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(){
-    int L = 5; // Número de linhas da matriz
-    int C = 3; // Número de colunas da matriz
-    int ocr[5][3]; // Matriz OCR
+int main() {
 
-    for(int i = 0 ; i < L ; i++){
-        for(int j = 0 ; j < C ; j++){
-            cin >> ocr[i][j];
-        }
-    }
+    int ocr[5][3]; // matriz do ocr
 
-    for(int i = 0 ; i < L ; i++){
-        for(int j = 0 ; j < C ; j++){
-            cout << ocr[i][j] << " ";
+    while (true) {
+
+        string codigo = ""; // código do número digitado na matriz
+        int soma = 0; // variável contadora
+
+        // leitura da matriz
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                cin >> ocr[i][j];
+
+                soma += ocr[i][j];
+
+                codigo += char(ocr[i][j] + '0'); // converte o código em char
+            }
         }
-        cout << endl;
+
+        // condição para parar
+        if (soma == 0) {
+            break; // sai do loop antes de imprimir qualquer coisa
+        }
+
+        // OCR - só será executado se a soma NÃO for 0
+        
+        // 0
+        if (codigo == "000010010010000") {
+            cout << "ZERO" << endl;
+        }
+
+        // 1
+        else if (codigo == "110110110110110") {
+            cout << "UM" << endl;
+        }
+
+        // 2
+        else if (codigo == "000110000011000") {
+            cout << "DOIS" << endl;
+        }
+
+        // 3
+        else if (codigo == "000110000110000") {
+            cout << "TRES" << endl;
+        }
+
+        // 4
+        else if (codigo == "010010000110110") {
+            cout << "QUATRO" << endl;
+        }
+
+        // 5
+        else if (codigo == "000011000110000") {
+            cout << "CINCO" << endl;
+        }
+
+        // 6
+        else if (codigo == "000011000010000") {
+            cout << "SEIS" << endl;
+        }
+
+        // 7
+        else if (codigo == "000110110110110") {
+            cout << "SETE" << endl;
+        }
+
+        // 8
+        else if (codigo == "000010000010000") {
+            cout << "OITO" << endl;
+        }
+
+        // 9
+        else if (codigo == "000010000110000") {
+            cout << "NOVE" << endl;
+        }
+
+        else {
+            cout << "DESCONHECIDO" << endl;
+        }
+
     }
 
     return 0;
