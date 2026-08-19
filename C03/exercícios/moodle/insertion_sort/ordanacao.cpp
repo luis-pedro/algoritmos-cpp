d38c893f16fd91656d63275d388ad48d6c596aa5
Luis Pedro Costa <luispedroccosta@gmail.com>
2026-08-19 14:50:53 -0300

    Exercício Moodle

---
+++ C03/exercícios/moodle/insertion_sort/ordanacao.cpp
@@ -0,0 +1,70 @@
+#include <cmath>
+#include "ordenacao.h"
+
+void insercaoDireta(int vetor[], int tamanho)
+{
+    int i, j; // contadores
+    int chave = 0;
+    for (j = 1; j < tamanho; j++)
+    {
+        chave = vetor[j];
+        i = j - 1;
+        while ((i >= 0) && (vetor[i] < chave))
+        {
+            vetor[i + 1] = vetor[i];
+            i = i - 1;
+        }
+        vetor[i + 1] = chave;
+    }
+}
+
+void bubbleSort(int vetor[], int tamanho)
+{
+    int i, j; // contadores
+    int trab;
+    bool troca;
+    int limite;
+    troca = true;
+    limite = tamanho - 1;
+    while (troca)
+    {
+        troca = false;
+        for (i = 0; i < limite; i++)
+            if (vetor[i] > vetor[i + 1])
+            {
+                trab = vetor[i];
+                vetor[i] = vetor[i + 1];
+                vetor[i + 1] = trab;
+                j = i;
+                troca = true;
+            }
+        limite = j;
+    }
+}
+
+void quickSort(int vetor[], int i, int j)
+{
+    int trab, esq, dir, pivo;
+    esq = i;
+    dir = j;
+    pivo = vetor[(int)round((esq + dir) / 2.0)];
+    do
+    {
+        while (vetor[esq] < pivo)
+            esq++;
+        while (vetor[dir] > pivo)
+            dir--;
+        if (esq <= dir)
+        {
+            trab = vetor[esq];
+            vetor[esq] = vetor[dir];
+            vetor[dir] = trab;
+            esq++;
+            dir--;
+        }
+    } while (esq <= dir);
+    if (dir - i >= 0)
+        quickSort(vetor, i, dir);
+    if (j - esq >= 0)
+        quickSort(vetor, esq, j);
+}
---
+++ C03/exercícios/moodle/insertion_sort/ordenacao.h
@@ -0,0 +1,10 @@
+#ifndef ORDENACAO_H
+#define ORDENACAO_H
+
+void insercaoDireta(int vetor[], int tamanho);
+
+void bubbleSort(int vetor[], int tamanho);
+
+void quickSort(int vetor[], int i, int j);
+
+#endif
